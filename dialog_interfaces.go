3e48fc5aba07ba77a22629dd9c51cf0cd5b6d193
Valeriy <valerys@radware.com>
2016-06-29 14:51:12 +0300

    implementing tests

--- dialog_interfaces.go
+++ dialog_interfaces.go
@@ -19,36 +19,36 @@ type ProgressIface interface {
 }
 
 type DialogIface interface {
-	// Shadow(truefalse bool)
-	// SetCancelLabel(label string)
-	// SetSize(height int, width int)
-	// SetTitle(title string)
-	// SetBackTitle(backtitle string)
-	// SetLabel(label string)
-	// SetOkLabel(label string)
-	// HelpButton(truefalse bool)
-	// SetHelpLabel(label string)
-	// SetExtraLabel(label string)
-	// Slider(min int, max int, step int) (int, error)
-	// Passivepopup(text string, timeout int)
+	Shadow(truefalse bool)
+	SetCancelLabel(label string)
+	SetSize(height int, width int)
+	SetTitle(title string)
+	SetBackTitle(backtitle string)
+	SetLabel(label string)
+	SetOkLabel(label string)
+	HelpButton(truefalse bool)
+	SetHelpLabel(label string)
+	SetExtraLabel(label string)
+	Slider(min int, max int, step int) (int, error)
+	Passivepopup(text string, timeout int)
 	Geticon() string
 	Getcolor() string
-	// Combobox(item ...string) (string, error)
-	// Calendar(date time.Time) (string, error)
-	// Checklist(listHeight int, tagItemStatus ...string) ([]string, error)
-	// Mixedform(title string, insecure bool, tagItemStatus ...string) ([]string, error)
-	// Fselect(filepath string) (string, error)
-	// Infobox(text string)
-	// Inputbox(value string) (string, error)
-	// Inputmenu(menuHeight int, tagItem ...string) ([]string, error)
-	// Menu(menuHeight int, tagItem ...string) (string, error)
-	// Msgbox(text string)
-	// Passwordbox(insecure bool) (string, error)
-	// Pause(seconds int)
-	// Textbox(filepath string) error
-	// Timebox(date time.Time) (string, error)
-	// Yesno() bool
-	// Radiolist(listHeight int, tagItemStatus ...string) (string, error)
-	// // Dselect(dirpath string) (string, error)
-	// Progressbar() ProgressIface
+	Combobox(item ...string) (string, error)
+	Calendar(date time.Time) (string, error)
+	Checklist(listHeight int, tagItemStatus ...string) ([]string, error)
+	Mixedform(title string, insecure bool, tagItemStatus ...string) ([]string, error)
+	Fselect(filepath string) (string, error)
+	Infobox(text string)
+	Inputbox(value string) (string, error)
+	Inputmenu(menuHeight int, tagItem ...string) ([]string, error)
+	Menu(menuHeight int, tagItem ...string) (string, error)
+	Msgbox(text string)
+	Passwordbox(insecure bool) (string, error)
+	Pause(seconds int)
+	Textbox(filepath string) error
+	Timebox(date time.Time) (string, error)
+	Yesno() bool
+	Radiolist(listHeight int, tagItemStatus ...string) (string, error)
+	Dselect(dirpath string) (string, error)
+	Progressbar() ProgressIface
 }
